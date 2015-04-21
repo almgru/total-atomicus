762bc92a05d3c309d7b80c3c332159b931005238
Daniel Alm Grundstrom <daniel.alm.grundstrom@live.se>
2015-04-21 14:07:44 +0200

    Renames project and adds licensing information

--- missile.js
+++ missile.js
@@ -1,7 +1,23 @@
 "use strict";
 /**
- * Created by daniel on 2015-04-20.
+ * Copyright 2015 Daniel Alm Grundström
+ *
+ * This file is part of Total Atomicus
+ *
+ * Total Atomicus is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Total Atomicus is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Total Atomicus. If not, see http://www.gnu.org/licenses/.
  */
+
 var Missile = function(game, x, y, id, targetX, targetY) {
     Phaser.Sprite.call(this, game, x, y, id);
 
--- preloader.js
+++ preloader.js
@@ -1,6 +1,21 @@
 "use strict";
 /**
- * Created by teh_daniel_37 on 4/18/15.
+ * Copyright 2015 Daniel Alm Grundström
+ *
+ * This file is part of Total Atomicus
+ *
+ * Total Atomicus is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Total Atomicus is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Total Atomicus. If not, see http://www.gnu.org/licenses/.
  */
 
 LDGame.Preloader = function(game) {
